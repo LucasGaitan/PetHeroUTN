b0d2c3c019530a2352585ee9a524bc1d97c45f3e
Santiago Dadan <santiagodadan@gmail.com>
2022-09-23 12:41:43 -0300

    basic styles
    Co-Authored-By: Ezequiel Morales <82302911+ezemoralesmdp@users.noreply.github.com>
    Co-Authored-By: Lucas Agustín Gaitan <85136443+LucasGaitan@users.noreply.github.com>

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" type="image/x-icon" href="./views/assets/favicon.ico">
+
     <link rel="stylesheet" href="./views/css/main.css">
 
     <title>PetHero</title>
@@ -16,7 +18,95 @@
 
 <body>
 
-    <h1>Hola Mundo!</h1>
+    <header class="header">
+
+        <nav class="header__nav">
+
+        <i class="fas fa-paw header__icon"></i>
+
+        <ul class="header__list">
+            <li class="header__item"><a class="header__link" href="#guardians">Guardians</a></li>
+            <li class="header__item"><a class="header__link" href="#owners">Owners</a></li>
+            <li class="header__item"><a class="header__link" href="#reviews">Reviews</a></li>
+            <li class="header__item"><a class="header__link" href="#us">About Us</a></li>
+        </ul>
+
+        <section class="header__section">
+
+            <p class="header__p"><a class="header__signIn" href="#">Sign In</a> <span class="header__span">/</span> <a class="header__signUp" href="#">Sign Up</a></p>
+
+        </section>
+
+        </nav>
+
+        <section class="header__body">
+
+            <h1 class="header__title">Pet Hero</h1>
+
+        </section>
+
+
+    </header>
+
+    <main class="main">
+
+        <section class="main__guardians" id="guardians">
+
+            <h3 class="main__section__title">Guardians</h3>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus odit tempora, ipsa tenetur dolorum porro quidem soluta quia voluptatum unde distinctio debitis voluptatem cupiditate ad minus? Magni illo ducimus deleniti nobis in dolor. Quidem sit sunt, quaerat eaque dolore consequatur, atque, fuga itaque sequi molestias quod? Totam fugit placeat obcaecati molestias minus laborum, earum rem recusandae voluptatem labore sed officiis ex, cupiditate dolore accusamus ipsum tempore vero nobis libero nulla. Necessitatibus similique excepturi consequatur cum beatae! Qui, repellat fugiat impedit harum doloremque nostrum fugit blanditiis id corporis sunt odio ad possimus ullam, molestias odit quaerat illum. Cum eum quas laudantium!</p>
+
+        </section>
+
+        <section class="main__owners" id="owners">
+
+            <h3 class="main__section__title">Owners</h3>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus odit tempora, ipsa tenetur dolorum porro quidem soluta quia voluptatum unde distinctio debitis voluptatem cupiditate ad minus? Magni illo ducimus deleniti nobis in dolor. Quidem sit sunt, quaerat eaque dolore consequatur, atque, fuga itaque sequi molestias quod? Totam fugit placeat obcaecati molestias minus laborum, earum rem recusandae voluptatem labore sed officiis ex, cupiditate dolore accusamus ipsum tempore vero nobis libero nulla. Necessitatibus similique excepturi consequatur cum beatae! Qui, repellat fugiat impedit harum doloremque nostrum fugit blanditiis id corporis sunt odio ad possimus ullam, molestias odit quaerat illum. Cum eum quas laudantium!</p>
+
+        </section>
+
+        <section class="main__reviews" id="reviews">
+
+            <h3 class="main__section__title">Reviews</h3>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus odit tempora, ipsa tenetur dolorum porro quidem soluta quia voluptatum unde distinctio debitis voluptatem cupiditate ad minus? Magni illo ducimus deleniti nobis in dolor. Quidem sit sunt, quaerat eaque dolore consequatur, atque, fuga itaque sequi molestias quod? Totam fugit placeat obcaecati molestias minus laborum, earum rem recusandae voluptatem labore sed officiis ex, cupiditate dolore accusamus ipsum tempore vero nobis libero nulla. Necessitatibus similique excepturi consequatur cum beatae! Qui, repellat fugiat impedit harum doloremque nostrum fugit blanditiis id corporis sunt odio ad possimus ullam, molestias odit quaerat illum. Cum eum quas laudantium!</p>
+
+        </section>
+
+        <section class="main__us" id="us">
+
+            <h3 class="main__section__title">About us</h3>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus odit tempora, ipsa tenetur dolorum porro quidem soluta quia voluptatum unde distinctio debitis voluptatem cupiditate ad minus? Magni illo ducimus deleniti nobis in dolor. Quidem sit sunt, quaerat eaque dolore consequatur, atque, fuga itaque sequi molestias quod? Totam fugit placeat obcaecati molestias minus laborum, earum rem recusandae voluptatem labore sed officiis ex, cupiditate dolore accusamus ipsum tempore vero nobis libero nulla. Necessitatibus similique excepturi consequatur cum beatae! Qui, repellat fugiat impedit harum doloremque nostrum fugit blanditiis id corporis sunt odio ad possimus ullam, molestias odit quaerat illum. Cum eum quas laudantium!</p>
+
+        </section>
+
+    </main>
+
+    <footer class="footer">
+
+    <section class="footer__logo">
+
+        <img src="./views/assets/doge.png" alt="" class="footer__icon">
+
+    </section>
+
+    <section class="footer__text">
+
+        <ul class="footer__list">
+            <li class="footer__info">asdasd</li>
+            <li class="footer__info">123123</li>
+            <li class="footer__info">asdasd</li>
+            <li class="footer__info">123123</li>
+        </ul>
+
+    </section>        
+
+    </footer>
+
+
+    <script src="https://kit.fontawesome.com/9682b774cb.js" crossorigin="anonymous"></script>
     
 </body>
 
