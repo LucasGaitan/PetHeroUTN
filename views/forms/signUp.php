9ad12a838686ecb3f293451ba8352e8172f8116c
LucasGaitan <lucasgaitan9112@gmail.com>
2022-09-27 11:50:13 -0300

    Forms
    -Sign In
    -Sign Up
    
    Co-Authored-By: Santiago Dadan <66185271+SantiagoDadan@users.noreply.github.com>
    Co-Authored-By: Ezequiel Morales <82302911+ezemoralesmdp@users.noreply.github.com>

---
+++ views/forms/signUp.php
@@ -0,0 +1,70 @@
+<?php
+require_once("./views/forms/signIn.php");
+?>
+
+<div class="modal fade" id="signUp" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalToggleLabel">Sign Up</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form class="row g-3">
+  <div class="col-md-6">
+    <label for="validationServer01" class="form-label">First name</label>
+    <input type="text" class="form-control" id="validationServer01" value="" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationServer02" class="form-label">Last name</label>
+    <input type="text" class="form-control " id="validationServer02" value="" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-12">
+    <label for="validationServerUsername" class="form-label">Username</label>
+    <div class="input-group has-validation">
+      <input type="text" class="form-control" id="validationServerUsername" aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
+      <div id="validationServerUsernameFeedback" class="invalid-feedback">
+        Please choose a username.
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-md-12">
+    <label for="validationServer03" class="form-label">Password</label>
+    <input type="password" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" required>
+    <div id="validationServer03Feedback" class="invalid-feedback">
+      Please provide a valid password.
+    </div>
+  </div>
+
+  <div class="col-md-12">
+    <label for="validationServer03" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" required>
+    <div id="validationServer03Feedback" class="invalid-feedback">
+      Please provide a valid password.
+    </div>
+  </div>
+
+
+
+  <div class="d-grid gap-2">
+    <button class="btn btn-primary " type="submit">Submit form</button>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+          <div class="d-grid gap-2 col-6 mx-auto">
+            
+            <button class="btn btn-primary mt-3 mb-3" data-bs-target="#signIn" data-bs-toggle="modal" data-bs-dismiss="modal">Sign In</button>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
--- views/header.php
+++ views/header.php
@@ -1,72 +1,82 @@
+<?php
+require_once("./views/forms/signUp.php");
+require_once("./views/forms/signIn.php");
+?>
 <!DOCTYPE html>
 
 <html lang="en">
 
 <head>
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&display=swap" rel="stylesheet">
 
-    <link rel="icon" type="image/x-icon" href="./views/assets/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="./views/assets/favicon.ico">
 
-    <link rel="stylesheet" href="./views/css/main.css">
+  <link rel="stylesheet" href="./views/css/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
 
 
 </head>
 
 <body>
 
-    <header class="header">
+  <header class="header">
 
-        <nav class="header__nav">
+    <nav class="header__nav">
 
-        <a href="#"><img class="header__icon" src="./views/assets/dogHero.png" alt=""></a>
+      <a href="#"><img class="header__icon" src="./views/assets/dogHero.png" alt=""></a>
 
-        <ul class="header__list">
-            <li class="header__item"><a class="header__link" href="#guardians">Guardians</a></li>
-            <li class="header__item"><a class="header__link" href="#owners">Owners</a></li>
-            <li class="header__item"><a class="header__link" href="#reviews">Reviews</a></li>
-            <li class="header__item"><a class="header__link" href="#us">About Us</a></li>
-        </ul>
+      <ul class="header__list">
+        <li class="header__item"><a class="header__link" href="#guardians">Guardians</a></li>
+        <li class="header__item"><a class="header__link" href="#owners">Owners</a></li>
+        <li class="header__item"><a class="header__link" href="#reviews">Reviews</a></li>
+        <li class="header__item"><a class="header__link" href="#us">About Us</a></li>
+      </ul>
 
-        <section class="header__section">
+      <section class="header__section">
 
-            <p class="header__p"><a class="header__signIn" href="#">Sign In</a><a class="header__signUp" href="#">Sign Up</a></p>
+        <p class="header__p"><a class="header__signIn" data-bs-toggle="modal" data-bs-target="#signIn" >Sign In</a>
+          <a class="header__signUp" data-bs-toggle="modal" data-bs-target="#signUp">Sign Up</a>
 
-        </section>
+        </p>
 
-        </nav>
+      </section>
 
-        <section class="header__body">
+    </nav>
 
-          <section class="header__body__content">
+    <section class="header__body">
 
-            <h1 class="header__title">Welcome to <br>Pet Hero!</h1>
+      <section class="header__body__content">
 
-            <div class="header__title__button">
-              <a href="#" class="header__title__button--text">Be a Hero</a>
-            </div>
-            
-          
-          </section>
+        <h1 class="header__title">Welcome to <br>Pet Hero!</h1>
 
-          <section class="header__body__img">
+        <div class="header__title__button">
+          <a href="#" class="header__title__button--text">Be a Hero</a>
+        </div>
 
-            <img src="./views/assets/mainImage.png" alt="">
 
-          </section>
+      </section>
 
-          <svg class="header__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#b41d78" fill-opacity="1" d="M0,224L720,160L1440,32L1440,320L720,320L0,320Z"></path>
-          </svg>
+      <section class="header__body__img">
 
-        </section>
+        <img src="./views/assets/mainImage.png" alt="">
 
-        <div class="header__arrow"></div>
+      </section>
 
-    </header>
+      <svg class="header__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#b41d78" fill-opacity="1" d="M0,224L720,160L1440,32L1440,320L720,320L0,320Z"></path>
+      </svg>
+
+    </section>
+
+    <div class="header__arrow"></div>
+
+  </header>
