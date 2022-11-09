c2c5a91d2337243c951f35f85cd10f3358b90213
Santiago Dadan <santiagodadan@gmail.com>
2022-11-09 11:39:19 -0300

    reservationList update
    Co-Authored-By: Ezequiel Morales <82302911+ezemoralesmdp@users.noreply.github.com>
    Co-Authored-By: Lucas Agustín Gaitan <85136443+LucasGaitan@users.noreply.github.com>

--- views/sections/reservationList.php
+++ views/sections/reservationList.php
@@ -71,14 +71,17 @@ $reservations = $reservationController->getAllReservationsByGuardianId();
 
 <section class="cardsContainer">
     <?php
-    foreach ($reservations as $value) {
+
+    if ($_SESSION['user']->getStartDate() != null && $_SESSION['user']->getEndDate() != null){
+        foreach ($reservations as $value) {
         ?>
         <div class="reservationCard__container">
-            <a href="<?php echo FRONT_ROOT?>Reservation/reservationSelected?idReservation=<?php echo $value["id_reservation"] ?>"
+            <a href="<?php echo FRONT_ROOT ?>Reservation/reservationSelected?idReservation=<?php echo $value["id_reservation"] ?>"
                class="reservationCard">
                 <div class="reservationCard__content">
                     <p class="reservationCard__title"><?php echo $value["ownerName"] ?></p>
-                    <p class="reservationCard__title"><?php echo $value["animalType"] ?>/<?php echo $value["animalBreed"] ?>/ <?php echo $value["animalSize"]?></p>
+                    <p class="reservationCard__title"><?php echo $value["animalType"] ?>
+                        /<?php echo $value["animalBreed"] ?>/ <?php echo $value["animalSize"] ?></p>
                     <p class="reservationCard__date"><?php echo $value["startDate"] ?>
                         / <?php echo $value["startDate"] ?></p>
                     <p class="reservationCard__salary"><?php if ($value["reservationState"] == 1) {
@@ -95,12 +98,38 @@ $reservations = $reservationController->getAllReservationsByGuardianId();
             </a>
         </div>
         <?php
+        }
+    }else{
+        ?>
+
+        <form action="<?php echo FRONT_ROOT ?>Guardian/setWorkDates" class="row g-2 justify-content-center" method="post">
+                    <div class="col-md-6">
+                        <label for="validationServer01" class="form-label">Start date</label>
+                        <input type="date" name="startDate" class="form-control" id="validationServer01" value="" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="validationServer02" class="form-label">End date</label>
+                        <input type="date" name="endDate" class="form-control " id="validationServer02" value="" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div class="d-grid gap-2 col-10">
+                        <button class="btn" style="background-color:#b41d78; color:#fff" type="submit">Confirm Date</button>
+                    </div>
+        </form><?php
+     
     }
     ?>
 
     <div class="cardsContainer__corner cardsContainer__corner--1"></div>
     <div class="cardsContainer__corner cardsContainer__corner--2"></div>
+
 </section>
+
 <section class="confirmReservation">
     <?php
 
